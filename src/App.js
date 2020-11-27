9bd44c9d76fde119c53efe1445ec662488e3203b
Rubén Rodríguez <rbn@iMac-de-Ruben.local>
2020-11-26 20:53:05 -0600

    deliver

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
   render(){
     return (
    <div className="App">
-        <h1>Iron contacts</h1>
+        <h1>Iron Contacts</h1>
         <button onClick={()=>this.addRandom()}>Add random contact</button>
         <button onClick={()=>this.sortName()}>Sort by name</button>
         <button onClick={()=>this.sortPopularity()}>Sort by popularity</button>
